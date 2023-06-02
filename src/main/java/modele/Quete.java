2a81bf5488d38f2b9d698016fd691d72c73641ff
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-02 07:08:16 +0200

    Ajout du package "modele", ajout de la classe "Quete" avec ses champs et l'entête de deux méthode de type "getter", getPos() et getPrecond() utile pour plus tard.

--- src/main/java/modele/Quete.java
+++ src/main/java/modele/Quete.java
@@ -1,4 +1,27 @@
 package modele;
 
 public class Quete {
+
+    private int numero;
+    private int [] pos = new int[2] ;
+    private int [] precond = new int[4];
+    private int duree;
+    private int experience;
+    private String intitule;
+
+    /**
+     * retoune la position de la quete sur la carte
+     * @return pos (int []) : position de la quete sur la carte
+     */
+    public int[] getPos() {
+        return pos;
+    }
+
+    /**
+     * retourne les préconditions de la quete
+     * @return precond (int []): préconditions de la quete
+     */
+    public int[] getPrecond() {
+        return precond;
+    }
 }
