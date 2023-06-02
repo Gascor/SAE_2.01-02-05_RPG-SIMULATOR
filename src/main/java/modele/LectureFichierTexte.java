a93788449dc8b66762586790d732a9147a7849b5
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-02 10:44:24 +0200

    Modification de "LectureFichierTexte.java", ajout de la méthode "lecture()".

--- src/main/java/modele/LectureFichierTexte.java
+++ src/main/java/modele/LectureFichierTexte.java
@@ -1,4 +1,22 @@
 package modele;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LectureFichierTexte {
+    public static void lecture(File fichier){
+        try{
+            Scanner scanner = new Scanner(fichier);
+            Scenario scenario = new Scenario(fichier.getName());
+            while (scanner.hasNext()){
+                Quete quete = new Quete(scanner.nextLine());
+                scenario.ajout(quete);
+
+            }
+        }
+        catch (FileNotFoundException e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
