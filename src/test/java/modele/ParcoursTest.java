15ff7e4563e60c8782ed94c768b7e1dad21198c8
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-04 14:56:27 +0200

    Ajout de la classe de test "ParcoursTest.java", ajout de l'entête de la méthode de test "extraitQuete()";

---
+++ src/test/java/modele/ParcoursTest.java
@@ -0,0 +1,13 @@
+package modele;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParcoursTest {
+
+    @Test @Order(1)
+    void extraitQuete(){
+    }
+}
