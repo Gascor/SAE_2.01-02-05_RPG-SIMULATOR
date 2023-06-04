1e9465d7e1fbc95d8d200e2e424dc9299db04de6
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-04 14:39:45 +0200

    Modification de la classe "Parcours.java", ajout des champs et du constructeur "Parcours()";

--- src/main/java/modele/Parcours.java
+++ src/main/java/modele/Parcours.java
@@ -1,4 +1,33 @@
 package modele;
 
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.TreeSet;
+
 public class Parcours {
+    private final Scenario chScenario;
+    private int chexp;
+    private int chduree;
+    private int chdeplacements;
+    private final String chComparatif;
+    private final LinkedHashMap<Integer,Quete> chQuetesFaite;
+    private final TreeSet<Quete> quetesNonFaite;
+    private final Quete queteFin;
+    private final HashSet<Quete> ensQuetePossible;
+    /**
+     * permet de constuire un objet de la classe Parcours
+     * @param parComparatif (String): permet de savoir comment les Parcours vont etre comparée
+     * @param parScenario (Scenario): Scenario sur lequel le parcours va se basé
+     */
+    public Parcours(Scenario parScenario,String parComparatif){
+        chScenario = parScenario;
+        chexp = 0;
+        chduree = 0;
+        chdeplacements = 0;
+        chComparatif = parComparatif;
+        chQuetesFaite =  new LinkedHashMap<>();
+        quetesNonFaite = extraitQuete(chScenario.getTreeQuete());
+        queteFin = chScenario.getTreeQuete().first();
+        ensQuetePossible = new HashSet<>();
+    }
 }
