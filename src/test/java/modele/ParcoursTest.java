33fdc0b2ad976197ae724801d2b59253165955f6
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-05 10:14:07 +0200

    Modification de "ParcoursTest.java", ajout de la méthode de test "quetesPossibles()";

--- src/test/java/modele/ParcoursTest.java
+++ src/test/java/modele/ParcoursTest.java
@@ -1,9 +1,6 @@
 package modele;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -74,5 +71,69 @@ import static org.junit.jupiter.api.Assertions.*;
         assertEquals(10,parcours.compareTo(parcours1));
         assertEquals(0,parcours.compareTo(parcours));
         assertEquals(-10,parcours1.compareTo(parcours));
+    }
+    @Test @Order(1)
+    public void quetesPossibles(){
+        System.out.println("Test méthode quetesPossible");
+        // Test 0
+
+        Scenario scenario = new Scenario("test1");
+        Quete quete1 = new Quete("1|(1, 1)|()|2|100|explorer pic de Bhanborim"); // Faite
+        Quete quete2 = new Quete("2|(1, 0)|()|2|100|explorer pic de Bhanborim"); // Possible
+        Quete quete3 = new Quete("3|(1, 0)|((4,),)|2|100|explorer pic de Bhanborim"); // Non Faite
+        Quete quete4 = new Quete("4|(1, 0)|((5,),)|2|100|explorer pic de Bhanborim"); // Non Faite
+        scenario.ajout(quete1);
+        scenario.ajout(quete2);
+        scenario.ajout(quete3);
+        scenario.ajout(quete4);
+        LinkedHashMap <Integer,Quete> queteFaites = new LinkedHashMap<>();
+        queteFaites.put(quete1.getNumero(),quete1);
+        HashSet<Quete> quetePossible = new HashSet<>();
+        quetePossible.add(quete2);
+        TreeSet<Quete> queteNonFaite = new TreeSet<>();
+        queteNonFaite.add(quete2);
+        queteNonFaite.add(quete3);
+        queteNonFaite.add(quete4);
+        Parcours parcours = new Parcours(scenario,0,0,0,"duree",queteFaites,queteNonFaite,quetePossible);
+        Quete [] solution = {quete2};
+        parcours.quetesPossibles();
+        assertArrayEquals(solution, parcours.getQuetePossible().toArray());
+        // Test 1
+        Scenario scenario1 = new Scenario("test");
+        Quete quete5 = new Quete("4|(1, 0)|((1,),)|2|100|explorer pic de Bhanborim"); // Non Faite, Quete 4bis
+        scenario1.ajout(quete1);
+        scenario1.ajout(quete2);
+        scenario1.ajout(quete3);
+        scenario1.ajout(quete5);
+        TreeSet<Quete> queteNonFaite1 = new TreeSet<>();
+        queteNonFaite1.add(quete2);
+        queteNonFaite1.add(quete3);
+        queteNonFaite1.add(quete5);
+        Parcours parcours1 = new Parcours(scenario1,0,0,0,"duree",queteFaites,queteNonFaite1,quetePossible);
+        HashSet<Quete> expectedSet = new HashSet<>();
+        expectedSet.add(quete2);
+        expectedSet.add(quete5);
+        parcours1.quetesPossibles();
+        Assertions.assertEquals(parcours1.getQuetePossible(), expectedSet);
+        // Test 2
+        Scenario scenario2 = new Scenario("test1");
+        Quete quete6 = new Quete("5|(1, 0)|((1,),)|2|100|explorer pic de Bhanborim");
+        scenario2.ajout(quete1);
+        scenario2.ajout(quete2);
+        scenario2.ajout(quete3);
+        scenario2.ajout(quete5);
+        TreeSet<Quete> queteNonFaite2 = new TreeSet<>();
+        queteNonFaite2.add(quete2);
+        queteNonFaite2.add(quete6);
+        queteNonFaite2.add(quete5);
+        Parcours parcours2 = new Parcours(scenario2,0,0,0,"duree",queteFaites,queteNonFaite2,quetePossible);
+
+        HashSet<Quete> expectedSet1 = new HashSet<>();
+        expectedSet1.add(quete2);
+        expectedSet1.add(quete5);
+        expectedSet1.add(quete6);
+        parcours2.quetesPossibles();
+        System.out.println(parcours2.getQuetePossible());
+        Assertions.assertEquals(parcours2.getQuetePossible(), expectedSet1);
     }
 }
