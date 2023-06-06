93b10ef47da8bb9de4af39dd63432d04c52fbb24
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-06 14:41:07 +0200

    Modification de "Classement.java", ajout de l'entête de la méthode "ajout()"

--- src/main/java/modele/Classement.java
+++ src/main/java/modele/Classement.java
@@ -5,4 +5,12 @@ import java.util.TreeMap;
 
 public class Classement {
     private static TreeMap<Integer, ArrayList<Parcours>> chTreeParcours = new TreeMap<>();
+
+    /**
+     * ajoute le parcours finis dans le classement
+     * @param parparcours (Parcours) est un parcours finis
+     */
+    public static void ajout(Parcours parparcours) {
+        ;
+    }
 }
