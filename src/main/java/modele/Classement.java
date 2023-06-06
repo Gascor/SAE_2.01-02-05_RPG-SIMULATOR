3ca19459cce4f796ae8dc1c932809effd485b2de
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-06 14:23:08 +0200

    Ajout de "Classement.java", ajout des champs;

---
+++ src/main/java/modele/Classement.java
@@ -0,0 +1,8 @@
+package modele;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class Classement {
+    private static TreeMap<Integer, ArrayList<Parcours>> chTreeParcours = new TreeMap<>();
+}
