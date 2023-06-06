eae02f8fe57fc52c68e7275900f45f73ce3eba0a
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-06 22:33:02 +0200

    Modification "LectureFichierTexte.java", modification de la méthode "lecture()";

--- src/main/java/modele/LectureFichierTexte.java
+++ src/main/java/modele/LectureFichierTexte.java
@@ -5,18 +5,25 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LectureFichierTexte {
-    public static void lecture(File fichier){
-        try{
+    /**
+     * lis un fichier scenario. Crée le scénario avec ses quetes
+     *
+     * @param fichier (File): le fichier du scénario
+     * @return  L'objet scenario : si le fichier existe
+     *          Null             : si le fichier n'existe pas
+     */
+    public static Scenario lecture(File fichier) {
+        try {
             Scanner scanner = new Scanner(fichier);
             Scenario scenario = new Scenario(fichier.getName());
-            while (scanner.hasNext()){
+            while (scanner.hasNext()) {
                 Quete quete = new Quete(scanner.nextLine());
                 scenario.ajout(quete);
-
             }
-        }
-        catch (FileNotFoundException e){
+            return scenario;
+        } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
+        return null;
     }
 }
