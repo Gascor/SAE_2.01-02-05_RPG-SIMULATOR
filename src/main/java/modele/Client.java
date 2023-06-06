ad1ac77f65a7478f2fb275e4b70bb44dbecff07d
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-06 22:35:13 +0200

    Modification "Client.java", ajout de la méthode principale "main()";

--- src/main/java/modele/Client.java
+++ src/main/java/modele/Client.java
@@ -1,4 +1,21 @@
 package modele;
 
+
+import java.io.File;
+
 public class Client {
+    public static void main(String[] a) throws ClassementException {
+        File file = new File("scenario"+File.separator+"scenario_0" +
+                ".txt");
+        Scenario scenario = LectureFichierTexte.lecture(file);
+        // lance la solution glutonne exhaustive
+        Algorithme.solutionGloutonneExhaustive(scenario);
+        try {
+            Classement.afficherClassement(1,1);
+        }
+        // affiche le message de l'exception
+        catch (ClassementException e) {
+            System.out.println(e.getCodeErreur());
+        }
+    }
 }
