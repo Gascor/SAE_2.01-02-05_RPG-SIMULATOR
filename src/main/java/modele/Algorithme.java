3c8856776e7e465cd74e85c1fa9ad540616fe589
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-06 15:47:57 +0200

    Modification "Algorithme.java", ajout des entêtes des méthodes "recursiviteGloutonneEfficace()" et "solutionGloutonneEfficace()";

--- src/main/java/modele/Algorithme.java
+++ src/main/java/modele/Algorithme.java
@@ -1,4 +1,22 @@
 package modele;
 
+import java.util.ArrayList;
+
 public class Algorithme {
+    /**
+     * permet de déterminer tous les parcours gloutons du scenario
+     * @param parScenario (Scenario): scenario qu'utilise les parcours
+     * @param listparcours ( ArrayList<Parcours>): liste chronologique des parcours ajoutés
+     */
+    public static void recursiviteGloutonneEfficace(Scenario parScenario,ArrayList<Parcours> listparcours){
+        ;
+    }
+    /**
+     * permet de lancer l'algorithme gluton efficace
+     *
+     * @param parScenario (Scenario): Scenario utilisé pour lancer l'algorithme
+     */
+    public static void solutionGloutonneEfficace(Scenario parScenario) {
+        ;
+    }
 }
