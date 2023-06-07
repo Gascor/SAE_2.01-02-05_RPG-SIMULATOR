242faeda0a5a8e92ebd2342bad2f9d1144d201a2
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 01:29:49 +0200

    Modification de "MenuBarre.java", ajout de la méthode MenuBarre(); Modification de "MenuBarre.java", extending MenuBar; Modification de "MenuBarre.java", implementing ConstantesScenario;

--- src/main/java/vue/MenuBarre.java
+++ src/main/java/vue/MenuBarre.java
@@ -1,4 +1,49 @@
 package vue;
 
-public class MenuBarre {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+
+import java.util.Optional;
+
+/**
+ * Classe représentant la barre de menu de l'interface utilisateur.
+ */
+public class MenuBarre extends MenuBar implements ConstantesScenario {
+    /**
+     * Constructeur de la classe MenuBarre.
+     * Initialise et configure les menus et les éléments de la barre de menu.
+     */
+    public MenuBarre() {
+        Menu menuQuitter = new Menu("Quitter");
+        MenuItem menuItemQuitter = new MenuItem("Quitter");
+        menuQuitter.getItems().add(menuItemQuitter);
+        Menu menuScenario = new Menu("Scenario");
+        this.getMenus().addAll(menuQuitter,menuScenario);
+        ToggleGroup scenarioGroupe = new ToggleGroup();
+        // Ajout des éléments de menu pour chaque scénario disponible
+        for (String numS :NSCENARIO){
+            RadioMenuItem menuItem = new RadioMenuItem(numS);
+            menuItem.setText(numS);
+            menuScenario.getItems().add(menuItem);
+            menuItem.setToggleGroup(scenarioGroupe);
+            menuItem.setOnAction(VBoxRoot.getControleur());
+        }
+        // Sélection du premier élément de menu par défaut
+        RadioMenuItem firstRadioMenuItem = (RadioMenuItem) scenarioGroupe.getToggles().get(0);
+        firstRadioMenuItem.setSelected(true);
+        // Gestionnaire d'événement pour l'option "Quitter"
+        menuItemQuitter.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Quitter l'application ?");
+                alert.setHeaderText("Êtes-vous certain de vouloir quitter l'application ?");
+                Optional<ButtonType> option = alert.showAndWait();
+                if (option.get() == ButtonType.OK) {
+                    System.exit(0);
+                }
+            }
+        });
+    }
 }
