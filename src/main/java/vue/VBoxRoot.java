413677ad7e6e5f172c90bf913c4d4fc4075bdf79
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 01:09:47 +0200

    Modification de "VBoxRoot.java", ajout des champs;

--- src/main/java/vue/VBoxRoot.java
+++ src/main/java/vue/VBoxRoot.java
@@ -1,7 +1,13 @@
 package vue;
 
+import controlleur.Controleur;
 import javafx.scene.layout.VBox;
 
-public class VBoxRoot extends VBox {
 
+public class VBoxRoot extends VBox {
+    private static ClassementBox classementBox;
+    private static ScenarioBox scenarioBox;
+    private static Controleur controleur;
+    private static Formulaire formulaire;
+    private static MenuBarre menuBarre;
 }
