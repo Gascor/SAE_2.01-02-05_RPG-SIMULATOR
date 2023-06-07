3374019ab9c5245fba2460e6f737348ff6ca6a81
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 01:46:33 +0200

    Modification de "ScenarioBox.java", ajout des méthodes "getScenario()" et "setScenarioActuelle()";

--- src/main/java/vue/ScenarioBox.java
+++ src/main/java/vue/ScenarioBox.java
@@ -1,4 +1,36 @@
 package vue;
 
-public class ScenarioBox {
+
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import modele.Launcher;
+import modele.Quete;
+import modele.Scenario;
+
+public class ScenarioBox extends VBox implements ConstantesScenario {
+    private Scenario[] listeScenario;
+    private Label labelScenario;
+    private TableView<Quete> tableQuete;
+
+    private Scenario scenarioActuelle;
+
+    /**
+     * Retourne le scénario actuellement sélectionné.
+     *
+     * @return Le scénario actuellement sélectionné.
+     */
+    public Scenario getScenario() {
+        return scenarioActuelle;
+    }
+
+    /**
+     * Définit le scénario actuellement sélectionné.
+     *
+     * @param numeroScenario Le numéro du scénario à sélectionner.
+     */
+    public void setScenarioActuelle(int numeroScenario) {
+        this.scenarioActuelle = listeScenario[numeroScenario];
+    }
 }
