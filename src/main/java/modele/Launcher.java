e03127d6145b98dfa9dcd979424b3e0d34e4f675
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-07 15:12:39 +0200

    Modification de "Launcher.java", ajout de la méthode "scenarioLoader()";

--- src/main/java/modele/Launcher.java
+++ src/main/java/modele/Launcher.java
@@ -4,4 +4,12 @@ import java.io.File;
 
 public class Launcher {
     public static Scenario[] listeScenario = new Scenario[11];
+    public static void scenarioLoader() {
+        for (int i = 0; i < 11; i++) {
+            String nomScenario = "scenario_" + i + ".txt";
+            File file = new File("scenario" + File.separator + nomScenario);
+            Scenario scenario = LectureFichierTexte.lecture(file);
+            listeScenario[i] = scenario;
+        }
+    }
 }
