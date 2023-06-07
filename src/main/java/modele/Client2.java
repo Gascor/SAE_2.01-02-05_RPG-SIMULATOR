f33b5dbe57f6325905b89133fc8069530cf2414d
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 00:10:02 +0200

    Ajout de "Client2.java";

---
+++ src/main/java/modele/Client2.java
@@ -0,0 +1,10 @@
+package modele;
+
+public class Client2 {
+    public static void main(String[] a){
+        try {
+            Launcher.mainView();
+        }
+        catch (LauncherException ignored){}
+    }
+}
