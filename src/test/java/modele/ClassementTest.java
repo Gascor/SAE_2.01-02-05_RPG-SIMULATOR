59ad30fef9261e36b4b32b79fb2206b2cc54fa8a
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 00:54:02 +0200

    Modification de "ClassementTest.java", ajout de la méthode de test "regulationParcours()";

--- src/test/java/modele/ClassementTest.java
+++ src/test/java/modele/ClassementTest.java
@@ -3,6 +3,7 @@ package modele;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -75,5 +76,50 @@ class ClassementTest {
         } catch (ClassementException e){
             System.out.println(e.getCodeErreur());
         }
+    }
+    @Test
+    void regulationParcours() {
+        Scenario s1 = LectureFichierTexte.lecture(new File("scenario"+File.separator+"scenario_0" +
+                ".txt"));
+        Parcours p1 = new Parcours(s1,0,8,0,"duree",new LinkedHashMap<>(),new TreeSet<>(),new HashSet<>());
+        Parcours p2 = new Parcours(s1,0,15,0,"duree",new LinkedHashMap<>(),new TreeSet<>(),new HashSet<>());
+        Classement.ajout(p1);
+        Classement.regulationParcours(2,p2,2);
+        ArrayList<Parcours> soluce1 = new ArrayList<>();
+        soluce1.add(p1);
+        soluce1.add(p2);
+        ArrayList<Parcours> sObtenu = new ArrayList<>();
+        sObtenu.add(Classement.getChTreeParcours().get(8).get(0));
+        sObtenu.add(Classement.getChTreeParcours().get(15).get(0));
+        assertEquals(soluce1,sObtenu);
+        Classement.getChTreeParcours().clear();
+        Classement.setChNbSolutionsTotales(0);
+        Classement.ajout(p2);
+        Classement.regulationParcours(1,p1,1);
+        ArrayList<Parcours> soluce2 = new ArrayList<>();
+        soluce2.add(p1);
+        assertEquals(soluce2,Classement.getChTreeParcours().get(8));
+        Classement.getChTreeParcours().clear();
+        Classement.setChNbSolutionsTotales(0);
+        Classement.ajout(p1);
+        Classement.regulationParcours(1,p2,1);
+        ArrayList<Parcours> soluce3 = new ArrayList<>();
+        soluce3.add(p1);
+        assertEquals(soluce3,Classement.getChTreeParcours().get(8));
+        Classement.getChTreeParcours().clear();
+        Classement.setChNbSolutionsTotales(0);
+        Classement.ajout(p2);
+        Classement.regulationParcours(1,p1,2);
+        ArrayList<Parcours> soluce4 = new ArrayList<>();
+        soluce4.add(p2);
+        assertEquals(soluce4,Classement.getChTreeParcours().get(15));
+        Classement.getChTreeParcours().clear();
+        Classement.setChNbSolutionsTotales(0);
+        Classement.ajout(p1);
+        Classement.regulationParcours(1,p2,2);
+        ArrayList<Parcours> soluce5 = new ArrayList<>();
+        soluce5.add(p2);
+        assertEquals(soluce4,Classement.getChTreeParcours().get(15));
+
     }
 }
