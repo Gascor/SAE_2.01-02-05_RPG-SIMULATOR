2c1ca8c9d252acaf75b5126747f666d10b46a9d9
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 08:31:37 +0200

    Ajout de la JavaDoc;

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.rpg_simulator","l":"controlleur"},{"m":"com.example.rpg_simulator","l":"modele"},{"m":"com.example.rpg_simulator","l":"vue"}];updateSearchResults();
