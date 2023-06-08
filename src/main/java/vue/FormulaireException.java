c7138587a9c225be00108dc62c94d97d3a9a47ed
Gascor <lucas-dasilvaferreira@hotmail.com>
2023-06-08 02:14:26 +0200

    Modification de "FormulaireException.java", ajout des méthodes "FormulaireException()" et "getCodeErreur()" et son champs; Modification de "FormulaireException.java", extending Exception; Modification de "FormulaireException.java", implementing ConstantesErreurs;

--- src/main/java/vue/FormulaireException.java
+++ src/main/java/vue/FormulaireException.java
@@ -1,4 +1,31 @@
 package vue;
 
-public class FormulaireException {
+import modele.ConstantesErreurs;
+
+/**
+ * Classe d'exception spécifique au formulaire.
+ * Cette exception est levée lorsqu'une erreur se produit lors de la validation du formulaire.
+ */
+public class FormulaireException extends Exception implements ConstantesErreurs {
+    private final int chCodeErreur;
+
+    /**
+     * Constructeur de la classe ClassementException.
+     *
+     * @param parCodeError le code d'erreur correspondant à l'exception
+     */
+    public  FormulaireException(int parCodeError) {
+        super();
+        chCodeErreur = parCodeError;
+    }
+
+    /**
+     * Retourne le code d'erreur associé à l'exception.
+     *
+     * @return le code d'erreur associé à l'exception
+     */
+    public String getCodeErreur() {
+        return ERREURS_FORMULAIRE[chCodeErreur];
+    }
 }
+
